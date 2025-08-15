95886ae7832bf877dff9afcb1ddfbd841436e4c3
CliffordWilsonK <cliffordwilsonk@gmail.com>
2025-08-15 04:57:40 -0400

    Third Commit

--- server.py
+++ server.py
@@ -3,12 +3,21 @@ from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask('Emotion Detector')
 
-@app.route('/emotionDetector', methods=['GET''])
+@app.route('/emotionDetector', methods=['GET'])
 def emotion_detection():
     text = request.args.get('textToAnalyze')
+    if not text or text.strip() == "":
+        return {"anger": None,"disgust": None,"fear": None,"joy": None,"sadness": None,"dominant_emotion": None}, 400
     result = emotion_detector(text)
-    response = f"For the given statement, the system response is 'anger': {result['anger']}, 'disgust': {result['disgust']}, 'fear': {result['fear']}, 'joy':{result['joy']} and 'sadness': {result['sadness']}. The dominant emotion is {result['dominant_emotion']}."
+
+    if result['dominant_emotion'] is None:
+        return "Invalid text! Please try again!", 400
+    response = (
+        f"For the given statement, the system response is 'anger': {result['anger']}, " f"'disgust': {result['disgust']}, 'fear': {result['fear']}, " f"'joy': {result['joy']} and 'sadness': {result['sadness']}. "
+ f"The dominant emotion is {result['dominant_emotion']}."
+    )
     return response
+
 
 @app.route('/')
 def render_index_page():
